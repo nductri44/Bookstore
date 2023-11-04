07bd83172377936935480b6c022f235032447ec5
nductri44 <nductri44@gmail.com>
2023-11-04 14:23:40 +0700

    Template update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
 5.times do
   Category.create(name: Faker::Book.genre, description: Faker::Lorem.sentence(word_count: 5))
 end
+
+categories = Category.all
+10.times do |n|
+  product = Product.create(
+    name: Faker::Book.title,
+    description: Faker::Lorem.sentence(word_count: 5),
+    author: Faker::Book.author,
+    publisher: Faker::Book.publisher,
+    price: Faker::Number.between(from: 100, to: 500),
+    stock: Faker::Number.between(from: 1, to: 100)
+  )
+  image = File.open(Rails.root.join("app/assets/images/book#{n + 1}.jpg"))
+  product.image.attach(io: image, filename: "book#{n + 1}.jpg")
+  product.categories << categories.sample(rand(1..5))
+  product.save!
+end
